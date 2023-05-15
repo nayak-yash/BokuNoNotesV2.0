d2f9ae072bda82f8a51785f529972bcfdd716514
Yash Kumar <97407569+nayak-yash@users.noreply.github.com>
2023-05-16 01:37:16 +0530

    Create Constants.kt

---
+++ app/src/main/java/com/bokuno/notes/utils/Constants.kt
@@ -0,0 +1,9 @@
+package com.bokuno.notes.utils
+
+class Constants {
+    companion object {
+        const val API_KEY = "YOUR_API_KEY"
+        const val BASE_URL = "https://api.openai.com/"
+        const val SEARCH_NOTES_TIME_DELAY = 1000L
+    }
+}
